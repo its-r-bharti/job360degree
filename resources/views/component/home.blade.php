08da1b1d4a689c3ae8076f367c12f806073504c9
prashant4093 <sharma.server4093@gmail.com>
2024-02-04 16:05:53 +0530

    branch

--- resources/views/component/home.blade.php
+++ resources/views/component/home.blade.php
@@ -19,6 +19,7 @@
 <section class="section-0 lazy d-flex bg-image-style dark align-items-center "   class="" data-bg="{{asset('assets/images/banner5.jpg')}}">
     <div class="container">
         <div class="row">
+            
             <div class="col-12 col-xl-8">
                 <h1>Find your dream job</h1>
                 <p>Thounsands of jobs available.</p>
